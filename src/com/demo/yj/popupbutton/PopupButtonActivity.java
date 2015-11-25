7de3406d9311c904fcf9b18df85f47124a54a4c7
Jing Yang <v_jing.yang@pactera.com>
2015-11-25 18:28:49 +0800

    #First Commit# Popup Button in Android

---
+++ src/com/demo/yj/popupbutton/PopupButtonActivity.java
@@ -0,0 +1,14 @@
+
+package com.demo.yj.popupbutton;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PopupButtonActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popup_button_activity);
+    }
+}
---
+++ src/com/demo/yj/popupbutton/PopupButtonView.java
@@ -0,0 +1,15 @@
+
+package com.demo.yj.popupbutton;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class PopupButtonView extends View {
+
+    public PopupButtonView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // TODO Auto-generated constructor stub
+    }
+
+}
